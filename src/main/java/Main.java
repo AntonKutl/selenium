8765733759a204df73e4762fb76ba964a385e0fa
Антон <write_me@live.ru>
2021-06-26 00:32:51 +0300

    Задание выполнено

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,4 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
@@ -12,63 +9,76 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
+
         System.setProperty("webdriver.chrome.driver",
                 "D:\\WebDriver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
         driver.manage().timeouts().
                 implicitlyWait(2, TimeUnit.SECONDS);
-        driver.get("https://www.avito.ru/vladivostok/orgtehnika_i_rashodniki?q=%D0%BF%D1%80%D0%B8%D0%BD%D1%82%D0%B5%D1%80");
+        driver.get("https://www.avito.ru");
 
-        waitTime(12);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
 
-       // driver.manage().window().maximize();
+        waitTime(5);
 
-       // System.out.println("Мы на странице " + driver.getTitle());
+        driver.manage().window().maximize();
 
+        System.out.println("Мы на странице " + driver.getTitle());
 
-        /*WebElement elementСategory = driver.findElement(By.id("category"));
+        //Выбрать в выпадающем списке “категория”  значение оргтехника и расходники
+        WebElement elementСategory = driver.findElement(By.id("category"));
         Select select = new Select(elementСategory);
         select.selectByVisibleText("Оргтехника и расходники");
 
-        waitTime(7);
-
+        //В поле поиск по объявлению ввести значение “Принтер”
         WebElement searchSearch = driver.findElement(By.id("search"));
         searchSearch.sendKeys("Принтер");
         searchSearch.sendKeys(Keys.RETURN);
 
-        waitTime(7);
 
+        //Нажать на поле город
         driver.findElement(By.className("main-text-2PaZG")).click();
 
-        waitTime(7);
+        waitTime(3);
 
+        //Заполнить значением “Владивосток” поле город  в открывшемся окне и кликнуть по первому предложенному варианту.
+        // Нажать на кнопку “Показать объявления”
         driver.findElement(By.className("suggest-input-3p8yi")).sendKeys("Владивосток");
-
-        waitTime(7);
-
+        waitTime(4);
         driver.findElement(By.className("suggest-suggests-bMAdj")).findElements(By.tagName("li")).get(0).click();
+        waitTime(3);
+        driver.findElement(By.className("popup-buttons-NqjQ3")).click();
+        waitTime(6);
 
-        waitTime(7);
+        //Прокрутка страницы
+        WebElement checkDelivery = driver.findElement(By.xpath("//div[@data-marker=\"delivery-filter/container\"]"));
+        js.executeScript("arguments[0].scrollIntoView();", checkDelivery);
 
-        driver.findElement(By.className("popup-buttons-NqjQ3")).click();*/
-
-
-
-/*         if (!driver.findElement(By.xpath("//span[contains(text(), 'С Авито Доставкой')]")).isSelected()){
-
+        //Проверить, активирован ли чекбокс, и если не активирован – активировать и нажать кнопку “Показать объявления”
+        if (!driver.findElement(By.xpath("//span[contains(text(), 'С Авито Доставкой')]")).isSelected()) {
             driver.findElement(By.xpath("//span[contains(text(), 'С Авито Доставкой')]")).click();
         }
 
+        waitTime(5);
 
-        waitTime(5);*/
+        //Эмуляция нажатия кнопки "Показать Х элементов"
+        driver.findElement(By.xpath("//button[contains(@data-marker,'search-filters')]")).click();
 
+        waitTime(5);
 
-        driver.findElement(By.xpath("//button[contains(@data-marker,'search-filters')])"));
+        //Выбор фильтрации по убыванию
+        WebElement elementSelect = driver.findElement(By.xpath("//select[contains(@class,'select-select-3CHiM')]"));
+        elementSelect.findElement(By.xpath("//option[. = 'Дороже']")).click();
 
+        //Печать в консоль название и цену 3 самых дорогих принтеров
+        List<WebElement> name = driver.findElements(By.xpath("//h3[contains(@itemprop,'name')]"));
+        List<WebElement> cost = driver.findElements(By.xpath("//span[contains(@class,'price-text-1HrJ_ text-text-1PdBw text-size-s-1PUdo')]"));
+        for (int i = 0; i < 3; i++) {
+            System.out.println(name.get(i).getText() + " цена " + cost.get(i).getText());
+        }
 
-
-        //driver.quit();
+        driver.quit();
 
     }
 
